5a078be39ac80ea6679d627917836b6868ec19fc
padma2015 <118128157+padma2015@users.noreply.github.com>
2023-04-10 11:09:36 +0530

    post method code

--- src/learnRA1.java
+++ src/learnRA1.java
@@ -10,12 +10,12 @@ public class learnRA1 {
 
 	public static void main(String[] args) {
 		RestAssured.baseURI="https://reqres.in";
-		RequestSpecification request1=RestAssured.given();
+		RequestSpecification request2=RestAssured.given();
 		JSONObject reqB=new JSONObject();
 		reqB.put("name", "morpheus");
 		reqB.put("job", "leader");
-		request1.body(reqB.toJSONString());
-		Response response=request1.post("api/users");
+		request2.body(reqB.toJSONString());
+		Response response=request2.post("api/users");
 		System.out.println(response.getBody().asString());
 		System.out.println(response.getStatusCode());
 		
